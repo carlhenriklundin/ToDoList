3ec1b2bc42ba744b5d6acce8a754b53bf54ee3ec
Carl-Henrik Lundin <lundin.carlhenrik@gmail.com>
2022-02-01 16:35:58 +0100

    Add project files.

---
+++ ToDoList/Data/ToDoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    class ToDoContext : DbContext
+    {
+        public DbSet<ToDo> ToDo { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server =.\SQLExpress; Database = ToDo; Trusted_Connection = True;");
+  
+        }
+
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ToDoList.Models;
+using ToDoList.Data;
+using System.Linq;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateNewToDo();
+
+
+            Console.WriteLine("Vilket Id vill du ta bort?");
+            int inputId = Convert.ToInt32(Console.ReadLine());
+            using (var database = new ToDoContext())
+            {
+                var deleteDB = database.ToDo.SingleOrDefault(td => td.Id == inputId);
+                database.Remove(deleteDB);
+                database.SaveChanges();
+            }
+
+
+            Console.WriteLine("Vilket Id vill du ändra till klart?");
+            inputId = Convert.ToInt32(Console.ReadLine());
+            using (var db = new ToDoContext())
+            {
+                var done = db.ToDo.SingleOrDefault(td => td.Id == inputId);
+                {
+                    done.Done = true;
+                    db.SaveChanges();
+                }
+            }
+
+
+        }
+
+        static void CreateNewToDo()
+        {
+            Console.WriteLine("Vad ska du göra?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Vem är ansvarig?");
+            string ansvarig = Console.ReadLine();
+
+            var todo = new ToDo()
+            {
+                Name = name,
+                Responsible = ansvarig
+            };
+
+
+            using (var database = new ToDoContext())
+            {
+                var todolistDB = database.ToDo;
+
+                todolistDB.Add(todo);
+                database.SaveChanges();
+            }
+        }
+    }
+}
